5ac78927aee22866073bf984bdecdac2b1d2107e
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-06-27 15:06:00 +0900

    refileの画像エラーにならないようにinitializersのapplicationコントローラーへおまじない記述

--- .history/config/initializers/application_controller_renderer_20200608110648.rb
+++ .history/config/initializers/application_controller_renderer_20200608110648.rb
@@ -6,5 +6,3 @@
 #     https: false
 #   )
 # end
-
-Refile.secret_key = '647122078b4aabfa6ae636d984c8e6b703742884096426c4f6b1567204265e436aa376aaf9c964bd0168ac288e3f113df9012b50f19beea6b68dfb0711c70b74'
