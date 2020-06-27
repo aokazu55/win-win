66e1e0a9ae0e2f34c5ba0c8c267bc50e0c7624dc
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-06-27 15:59:15 +0900

    omniauthのコールバック用コントローラーの編集

---
+++ .history/app/controllers/users/omniauth_callbacks_controller_20200627155834.rb
@@ -0,0 +1,28 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+	def google_oauth2
+		@user = User.find_for_google(request.env['omniauth.auth'])
+		if @user.persisted?
+			sign_in_and_redirect @user, event: :authentication
+			set_flash_message(:notice, :success, kind: "google") if is_navigational_format?
+		else
+			session['devise.google_data'] = request.env['omniauth.auth']
+			redirect_to new_user_registration_url
+		end
+	end
+
+	def facebook
+		@user = User.find_for_facebook(request.env['omniauth.auth'])
+		if @user.persisted?
+			sign_in_and_redirect @user, event: :authentication
+			set_flash_message(:notice, :success, kind: "facebook") if is_navigational_format?
+		else
+			session['devise.facebook_data'] = request.env['omniauth.auth']
+			redirect_to new_user_registration_url
+		end
+	end
+
+	def failure
+		redirect_to root_path
+	end
+end
