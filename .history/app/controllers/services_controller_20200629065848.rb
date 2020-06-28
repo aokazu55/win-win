7ec720d61d0433f0d8efe13310008d2f3ec6f1bf
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-06-29 07:30:11 +0900

    kaminari

---
+++ .history/app/controllers/services_controller_20200629065848.rb
@@ -0,0 +1,57 @@
+class ServicesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_service, only: [:show, :edit, :update]
+  PER = 12
+
+    def new
+      @service = Service.new
+    end
+
+    def create
+      @service = current_user.services.build(service_params)
+      if @service.save
+        redirect_to service_path(@service), notice: "サービスを投稿しました。"
+      else
+        render :new, alert: "サービス投稿に失敗しました。。。"
+      end
+    end
+
+    def index
+      @services = Service.all.page(params[:page]).per(PER)
+    end
+
+    def show
+      @like = current_user.likes.find_by(service_id: @service.id)
+      @likes= @service.like_users
+    end
+
+    def edit
+      if @service.user_id != current_user.id
+          redirect_to services_path, alert: "他ユーザーの情報は変更出来ません。"
+      end    
+    end
+
+    def update
+      if @service.update(service_params)
+        redirect_to service_path(@service), notice: "サービスを更新しました。"
+      else
+        render :edit, alert: "サービスを更新に失敗しました。"
+      end
+    end
+
+    def destroy
+      service = Service.find(params[:id])
+      service.destroy
+      redirect_to user_path(service.user), notice: "サービスを削除しました。"
+    end
+
+  private
+
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
+  def service_params
+    params.require(:service).permit(:main_service_id, :service_detail_id, :service_name, :price, :working_time, :period_start, :period_end, :image, :confidence_level_id, :remark, :transportation_expenses)
+  end
+end
---
+++ .history/app/controllers/users_controller_20200629071709.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_user, only: [:show, :edit, :update]
+  per = 16
+
+    def index
+      @users = User.page(params[:page]).per
+    end
+
+    def show
+    end
+
+    def edit
+      if @user != current_user
+          redirect_to user_path(current_user), alert: "不正なアクセスです。"
+      end
+    end
+
+    def update
+      if @user.update(user_params)
+        redirect_to user_path(@user), notice: "ユーザー情報を更新しました。"
+      else
+        render :edit
+      end
+    end
+
+    def following
+      @title = "Following"
+      @user  = User.find(params[:id])
+      @users = @user.following
+      render 'show'
+    end
+
+    def followers
+      @title = "Followers"
+      @user  = User.find(params[:id])
+      @users = @user.followers
+      render 'show'
+    end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :image)
+  end
+
+end
